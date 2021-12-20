54837f8a904d821a1c49e3e8ec8892a152867276
troybougere <96433901+troybougere@users.noreply.github.com>
2021-12-20 10:53:32 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "vsphere_user" {
+  type = string
+  description = "the username for vsphere"
+}
+
+variable "vsphere_password" {
+  type = string
+  description = "The password for vsphere"
+}
+
+variable "vsphere_server" {
+  type = string
+  description = "the hostname or ip address of your vcenter server"
+}
